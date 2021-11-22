b0dc6b53646d32b5b3baa44ea369a36ec84bc679
Naziya Zhakupova <sabrkyzy@gmail.com>
2021-11-22 22:27:36 +0900

    add dependency

--- src/parse.py
+++ src/parse.py
@@ -26,11 +26,32 @@ def class_in_module ( module: str):
 def function_in_module ( module: str ):
     return lambda member: isfunction( member ) and member.__module__ == module.__name__
     
+def is_protected ( function_name ):
+    return function_name.startswith('_')
+
+def add_dependency(klass, analyzed_classes):
+    if klass in analyzed_classes:
+        print("Analyzed class")
+        return analyzed_classes
+    analyzed_classes.add(klass)
+    
+    return analyzed_classes
+
+    
+
 if __name__ == "__main__":
     sys.path.append(str(Path().parent.absolute()))
-    module = importlib.import_module("examples.example")
+    module = importlib.import_module("examples.queue_example")
     classes_in_module = getmembers( module, class_in_module( module ) )
     functions_in_module =  getmembers( module, function_in_module( module ) )
+    analyzed_classes = list()
+    for class_name, klass in classes_in_module:
+        analyzed_classes = add_dependency( klass, analyzed_classes )
+    for function_name, func in functions_in_module:
+        if is_protected(function_name):
+            continue
+            
+
 
 
 
