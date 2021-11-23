bcd4f41781bb3c830850027ab5da1e1343b8f06a
Shy <chikaphys9@gmail.com>
2021-11-24 02:55:44 +0900

    generate_clusters simple done

--- src/parse.py
+++ src/parse.py
@@ -11,41 +11,39 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Optional
 import typing
 import inspect
+import utils
+
 
 class Function(object):
     """
     class representing simple functions
     """
-
     def __init__(self, function_name, function, ftype=None):
         self.function_name = function_name
         self.function = function
         self.ftype = ftype
 
-    def generator(self):
-        pass
 
-class Method (object):
-    def __init__(self, method_name, method, mtype = None):
+class Method(object):
+    def __init__(self, method_name, klass, method, mtype=None):
         self.method_name = method_name
+        self.klass = klass
         self.method = method
         self.mtype = mtype
 
 
 class Constructor(object):
-    def __init__(self, klass, ctype = None):
+    def __init__(self, klass, klass_name, ktype=None):
         self.klass = klass
-        self.ctype = ctype
-
-    def get_type(self):
-        return self.ctype
+        self.klass_name = klass_name
+        self.ktype = ktype
 
 
 def infer_type(function):
     """
 
     :param function:
-    :return: the type information of the function or method
+    :return: the type information of the callable
     """
     signature = inspect.signature(function)
     parameters = {}
@@ -61,6 +59,7 @@ def infer_type(function):
 
     return signature, parameters, ret_type
 
+
 def wrap_var_param_type(type_, param_kind):
 
     if param_kind == inspect.Parameter.VAR_POSITIONAL:
@@ -74,72 +73,156 @@ def wrap_var_param_type(type_, param_kind):
     return type_
 
 
-class Foo():
-    pass
-
-
-def add(a: Foo, b: Foo) -> Foo:
-    return a + b
-
-
 def class_in_module(module):
     return lambda member: isclass(member) and member.__module__ == module.__name__
 
 
 def function_in_module(module: str):
-    return lambda member: isfunction( member ) and member.__module__ == module.__name__
-    
-def is_protected ( function_name ):
-    return function_name.startswith('_') and not function_name.startswith('__')
-
-
-def method_defined_in_class ( klass, method ):
-    #get class that defined method
-    print( method )
-    print( ismethod( method ) )
-    if ( ismethod( method)):
-        print( getmro(method.__self__.__class__))
-        # elif (isfunction( method )):
-
-    # defined_class =
-    return True
+    return lambda member: isfunction(member) and member.__module__ == module.__name__
 
 
 def is_protected(function_name):
-    return function_name.startswith('_')
+    return function_name.startswith('_') and not function_name.startswith('__')
 
 
-def add_dependency(klass, analyzed_classes):
-    if klass in analyzed_classes:
-        print("Analyzed class")
-        return analyzed_classes
-    analyzed_classes.append(klass)
-    # construct = Constructor(klass, infer_type(klass.__init__))
-    #add dependency
-    for method_name, method in getmembers(klass, isfunction):
-        # method = Method(method_name, method, infer_type(method))
-        print( method_name)
-        method_defined_in_class( klass, method)
-        if ( is_constructor( method ) or is_protected( method_name ) or not method_defined_in_class( klass, method)):
-            continue
-    return analyzed_classes
+def is_constructor(method_name: str) -> bool:
+    return method_name == "__init__"
 
-        
+
+def method_defined_in_class(method, klass):
+    return klass == utils.get_class_that_defined_method(method)
+
+
+# def add_class(klass, klass_name, recursion_level, analyzed_classes):
+#
+#     assert(inspect.isclass(klass))
+#     methods = []
+#     if klass in analyzed_classes:
+#         return
+#     if hasattr(klass, "__init__"):
+#         constructor = Constructor(klass, klass_name, infer_type(klass.__init__))
+#     else:
+#         constructor = Constructor(klass, klass_name, (None, {}, None))
+#
+#     for method_name, method in inspect.getmembers(klass, inspect.isfunction):
+#
+#         m = Method(method_name, klass, method, infer_type(method))
+#
+#         if is_constructor(method_name) or is_protected(method_name) or \
+#                 not method_defined_in_class(method, klass):
+#             continue
+#         methods.append(m)
+
+
+class TestCluster:
+    """
+    Cluster that collects and organizes methods, functions, classes for
+    further generation
+    """
+    def __init__(self):
+        self.generators = dict()
+        self.modifiers = dict()
+        self.objects_under_test = set()
+        self.analyzed_classes = set()
+
+    def generate_cluster(self, module_name, pkg=None):
+        module = importlib.import_module(module_name, pkg)
+        classes_in_module = getmembers(module, class_in_module(module))
+        functions_in_module = getmembers(module, function_in_module(module))
+        for function_name, func in functions_in_module:
+            if is_protected(function_name):
+                continue
+            fun = Function(function_name, func, infer_type(func))
+            self.add_function(fun, True)
+
+        for klass_name, klass in classes_in_module:
+            self.add_class(klass, klass_name, 1, True)
+
+    def add_class(self, klass, klass_name, recursion_level, include):
+
+        assert (inspect.isclass(klass))
+        # methods = []
+        if klass in self.analyzed_classes:
+            return
+        if hasattr(klass, "__init__"):
+            constructor = Constructor(klass, klass_name, infer_type(klass.__init__))
+        else:
+            constructor = Constructor(klass, klass_name, (None, {}, None))
+
+        self.add_constructor(constructor, include)
+
+        for method_name, method in inspect.getmembers(klass, inspect.isfunction):
+
+            temp_method = Method(method_name, klass, method, infer_type(method))
+
+            if is_constructor(method_name) or is_protected(method_name) or \
+                    not method_defined_in_class(method, klass):
+                continue
+            self.add_method(temp_method, include)
+
+    def add_function(self, function: Function, include):
+        ret_type = function.ftype[2]
+
+        if include:
+            self.objects_under_test.add(function)
+
+        if ret_type is None or ret_type is type(None) or ret_type in utils.PRIMITIVES:
+            return
+
+        if not (ret_type in self.generators):
+            self.generators[ret_type] = set()
+        self.generators[ret_type].add(function)
+
+    def add_method(self, method, include):
+        ret_type = method.mtype[2]
+
+        if include:
+            self.objects_under_test.add(method)
+
+        if ret_type is None or ret_type is type(None) or ret_type in utils.PRIMITIVES:
+            return
+
+        if not (ret_type in self.generators):
+            self.generators[ret_type] = set()
+        self.generators[ret_type].add(method)
+
+    def add_constructor(self, constructor, include):
+        ret_type = constructor.klass
+
+        if include:
+            self.objects_under_test.add(constructor)
+
+        if ret_type is None or ret_type is type(None) or ret_type in utils.PRIMITIVES:
+            return
+
+        if not (ret_type in self.generators):
+            self.generators[ret_type] = set()
+        self.generators[ret_type].add(constructor)
+
+        # TODO: resolve dependencies
+
+
 if __name__ == "__main__":
     sys.path.append(str(Path().parent.absolute()))
     sys.path.append(str(Path().parent.absolute()) + "\\examples")
-    test_module = importlib.import_module("examples.example")
-    classes_in_module = getmembers(test_module, class_in_module(test_module))
-    functions_in_module = getmembers(test_module, function_in_module(test_module))
-    one_function = functions_in_module[0][1]
-    analyzed_classes = list()
-    t = infer_type(one_function)
-    for function_name, func in functions_in_module:
-        if is_protected(function_name):
-            continue
-
-    for _, klass in classes_in_module:
-        analyzed_classes = add_dependency(klass, analyzed_classes)
+    t = TestCluster()
+    t.generate_cluster("examples.example")
+    # test_module = importlib.import_module("examples.example")
+    # classes_in_module = getmembers(test_module, class_in_module(test_module))
+    # functions_in_module = getmembers(test_module, function_in_module(test_module))
+    # one_function = functions_in_module[0][1]
+    # analyzed_classes = list()
+    # t = infer_type(one_function)
+    # for function_name, func in functions_in_module:
+    #     if is_protected(function_name):
+    #         continue
+    #     f = Function(function_name, func, infer_type(func))
+    #     # add function to cluster
+    # for _, klass in inspect.getmembers(module, class_in_module(self._module_name)):
+    #     self._add_dependency(klass, 1, True)
+    #
+    # for _, klass in classes_in_module:
+    #     analyzed_classes = add_dependency(klass, analyzed_classes)
 
 
 
---
+++ src/utils.py
@@ -0,0 +1,35 @@
+from typing import Optional
+import inspect
+import types
+
+PRIMITIVES = [int, str, bytes, bool, float, complex]
+COLLECTIONS = [list, set, tuple, dict]
+
+
+def get_class_that_defined_method(method: object) -> Optional[object]:
+    """Retrieves the class that defines a method.
+
+    Taken from https://stackoverflow.com/a/25959545/4293396
+
+    Args:
+        method: The method
+
+    Returns:
+        The class that defines the method
+    """
+    if inspect.ismethod(method):
+        assert isinstance(method, types.MethodType)
+        for cls in inspect.getmro(method.__self__.__class__):
+            if cls.__dict__.get(method.__name__) is method:
+                return cls
+        method = method.__func__  # fallback to __qualname__ parsing
+    if inspect.isfunction(method):
+        assert isinstance(method, types.FunctionType)
+        module = inspect.getmodule(method)
+        attribute_name = method.__qualname__.split(".<locals>", 1)[0].rsplit(".", 1)[0]
+        if not hasattr(module, attribute_name):
+            return None
+        cls = getattr(module, attribute_name)
+        if isinstance(cls, type):
+            return cls
+    return getattr(method, "__objclass__", None)
